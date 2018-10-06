3555121d6ca2fd0d303eee301940eb5a9c5d4b5d
sakai59 <s1411146@u.tsukuba.ac.jp>
2018-10-06 17:39:21 +0900

    node_moduleを取り除いた

--- node_modules/phina.js/gulp/tasks/docs.js
+++
@@ -1,7 +0,0 @@
-var gulp    = require('gulp');
-var shell   = require('gulp-shell');
-
-
-gulp.task('docs', shell.task([
-  'jsduck ./src --output ./docs --title "phina.js docs"',
-]));
--- node_modules/phina.js/gulp/tasks/download.js
+++
@@ -1,9 +0,0 @@
-// なんかダウンロードするときに使えそう
-
-var gulp      = require('gulp');
-var download  = require('gulp-download');
-
-gulp.task('download', function() {
-  download('http://tmlife.net')
-    .pipe(gulp.dest('downloads/'));
-});
